fa9ea1c7c37d7ac16e94848c53713c7f6a83a14f
ashoaa <a.shoaahaghighi@gmail.com>
2023-06-24 23:17:05 +0330

    Add loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import Item from "../components/Item.jsx";
+import axios from "axios";
+import { useLoaderData } from "react-router-dom";
 
 const Home = () => {
+  const data = useLoaderData();
+
   return (
     <>
-      <Item />
+      <Item data={data} />
     </>
   );
 };
 
 export default Home;
+export const loader = async () => {
+  const response = await axios.get("https://fakestoreapi.com/products/");
+  return response.data;
+};
